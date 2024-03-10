d3cc2f3b0aac2f85c83239168b3734e155801dcd
Eb0b0l0 <89797412+ebobo54@users.noreply.github.com>
2024-03-10 23:24:42 +0500

    Update lab_12.py

--- lab_12/lab_12.py
+++ lab_12/lab_12.py
@@ -1,18 +1,16 @@
 import pyforms
-from pyforms import BaseWidget
+from pyforms.gui.basewidget import BaseWidget
 from pyforms.controls import ControlText
-from pyforms.controls import ControlButton
 
 class lab_12(BaseWidget):
 
     def __init__(self):
         super().__init__('Элементарные частицы')
 
-        self._electron =ControlText =ControlText('Електрон')
-        self._neitron =ControlText =ControlText('Нейтроны')
-        self._proton =ControlText =ControlText('Протоны')
+        self._electron = ControlText('Електрон')
+        self._neutron = ControlText('Нейтрон')
+        self._proton = ControlText('Протон')
 
 
-    # from pyforms import start_app
-
-if __name__ =="__main__": pyforms.start_app(lab_12)
+if __name__ == "__main__":
+    pyforms.start_app(lab_12)
